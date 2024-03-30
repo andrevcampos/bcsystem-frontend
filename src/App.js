487d7fce9105b3902969d9400ef3a8e58d0f3e71
Andre Campos <andrevcampos@hotmail.com>
2024-03-30 22:12:59 +1000

    first

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from 'react'
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(()=> {
+    fetch("/api/v1/user/test").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
