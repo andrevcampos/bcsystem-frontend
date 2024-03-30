e753da5afcb6c310cf924cab5844fffada441ef1
Andre Campos <andrevcampos@hotmail.com>
2024-03-31 08:30:07 +1000

    update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, {useEffect, useState} from 'react'
 
 function App() {
 
-  const [backendData, setBackendData] = useState([{}])
+  // const [backendData, setBackendData] = useState([{}])
 
-  useEffect(()=> {
-    fetch("/api/v1/user/test").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
+  // useEffect(()=> {
+  //   fetch("/api/v1/user/test").then(
+  //     response => response.json()
+  //   ).then(
+  //     data => {
+  //       setBackendData(data)
+  //     }
+  //   )
+  // }, [])
 
   return (
     <div>App2</div>
