3e5320918ae3a3744bd4ebbe08d5f90ded204ee5
Andre Campos <andrevcampos@hotmail.com>
2024-04-04 19:56:50 +1000

    fix url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [backendData, setBackendData] = useState([{}])
 
   useEffect(() => {
-    fetch("https://bcsystem.app/api/v1/user/test").then(
+    fetch("http://api.bcsystem.app/api/v1/user/test").then(
       response => response.json()
     ).then(
       data => {
